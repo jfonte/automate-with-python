b4fa0a85b614b5649c6a9e6a8260ca75d35004c4
Josue Fontecha <jfontecha@gmail.com>
2015-12-21 10:07:18 +0100

    solved proj2 unit4 by transposing matrix with zip

--- unit4-lists/practice-projects/ex2-character-pic-grid.py
+++ unit4-lists/practice-projects/ex2-character-pic-grid.py
@@ -9,7 +9,40 @@ grid = [['.', '.', '.', '.', '.', '.'],
         ['O', 'O', 'O', 'O', '.', '.'],
         ['.', 'O', 'O', '.', '.', '.'],
         ['.', '.', '.', '.', '.', '.']]
+
 You can think of grid[x][y] as being the character at the x- and y-coordinates of a “picture” drawn with text characters. The (0, 0) origin will be in the upper-left corner, the x-coordinates increase going right, and w the y-coordinates increase going down.”
 
+Copy the previous grid value, and write code that uses it to print the image.
+..OO.OO..
+.OOOOOOO.
+.OOOOOOO.
+..OOOOO..
+...OOO...
+....O....”
+
 '''
-print('fuck you')
+
+# so basically this is a matrix, and all I need to do is move everything in the first column to the first row, 2nd col -> 2nd row
+
+grid = [['.', '.', '.', '.', '.', '.'],
+        ['.', 'O', 'O', '.', '.', '.'],
+        ['O', 'O', 'O', 'O', '.', '.'],
+        ['O', 'O', 'O', 'O', 'O', '.'],
+        ['.', 'O', 'O', 'O', 'O', 'O'],
+        ['O', 'O', 'O', 'O', 'O', '.'],
+        ['O', 'O', 'O', 'O', '.', '.'],
+        ['.', 'O', 'O', '.', '.', '.'],
+        ['.', '.', '.', '.', '.', '.']]
+
+new_grid=[]
+
+# transpose matrix
+
+def translate_this(old_grid):
+	for cols in zip(*old_grid):
+		print(' '.join(cols))
+
+try:
+	translate_this(grid)
+except:
+	pass
